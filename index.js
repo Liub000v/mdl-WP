d35d74e48365c6838519185ea7430162406b9ccc
Liub000v <62075023+Liub000v@users.noreply.github.com>
2021-12-12 22:55:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import axios from 'axios';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 app.get('/wordpress/', async (req, res, next) => { 
     const content = req.query.content; 
-    const resp = await axios.post(
+    const response = await axios.post( 
         'https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token', 
         { username: 'gossjsstudent2017', password: '|||123|||456' }, 
     ); 
-    const token = resp.data.token;
+    const token = response.data.token; 
     
-    const wpRes = await axios.post(
+    const wordpressResponse = await axios.post( 
         'https://wordpress.kodaktor.ru/wp-json/wp/v2/posts', 
-        { content, title: 'liubov97', status: 'publish' },
+        { content, title: 'liubov97', status: 'publish' }, 
         { 
             headers: { 
                 Authorization: `Bearer ${token}`, 
@@ -22,7 +22,7 @@ app.get('/wordpress/', async (req, res, next) => {
         }, 
     ); 
     
-    res.send(wpRes.data.id + '');
+    res.send(wordpressResponse.data.id + ''); 
     })
 
     .all("/login", (r) => r.res.send("liubov97"))
@@ -30,6 +30,4 @@ app.get('/wordpress/', async (req, res, next) => {
     .listen(PORT, ()=>{
         console.log('Server has been started...');
     });
-
-
     
